79103df7b0345b333bd325a3cce08a180ee1952f
DDR144 <luis.fernando.didier.hr@gmail.com>
2025-05-31 03:07:04 -0500

    Tecnico Completado

---
+++ src/main/java/com/peru/smartperu/repository/TecnicoRepository.java
@@ -0,0 +1,7 @@
+package com.peru.smartperu.repository;
+
+import com.peru.smartperu.model.Tecnico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TecnicoRepository extends JpaRepository<Tecnico, Integer> {
+}
